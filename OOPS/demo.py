97a0ff8c5a9803096139133ebfd15b3c65a895f7
Shabeen <shabeenshazz313@gmail.com>
2024-09-10 07:34:46 +0530

    created student class and their instance

--- OOPS/demo.py
+++ OOPS/demo.py
@@ -1,3 +1,36 @@
-print("object oriented programming")
+# object oriented programming
 
-print("this is my oops demo")
+# class [blueprint, Design pattern, Template]
+# object [ instance | real world entity]
+
+
+class student:
+
+    name:str
+    age:int
+    address:str
+    contact:int
+    course:str
+    studid:str
+
+    def set_student(self,name,age,address,contact,course,id):
+
+        self.name=name
+        self.age=age
+        self.address=address
+        self.course=course
+        self.studid=id
+        self.contact=contact
+
+    def display_student(self):
+
+        print(self.name,self.age,self.address,self.contact,self.course,self.studid)
+
+
+# creating object of student class 
+
+student_instance=student()
+
+student_instance.set_student("shabeen",12,"Akkaraparmbil house",9207251721,"python",101)
+
+display=student_instance.display_student()
